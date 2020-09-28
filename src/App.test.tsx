166ec81760d3dba4b12e21bdb46b8397bcc7eccd
oleksii.semko <oleksii.semko@gmail.com>
2020-09-29 00:46:37 +0300

    Base test file fixed. Not needed images removed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  const { container } = render(<App />);
+
+  test('renders header section', () => {
+    const headerSection = container.querySelector('header');
+
+    expect(headerSection).toBeInTheDocument();
+    expect(headerSection).toHaveTextContent(/React related repositories list/i);
+
+  });
 });
