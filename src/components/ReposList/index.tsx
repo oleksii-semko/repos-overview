e2a3b1d2f91c0bec5925a88afe008c9044e4fe50
oleksii.semko <oleksii.semko@gmail.com>
2020-09-29 00:12:35 +0300

    Alert message used for data fetch error case

--- src/components/ReposList/index.tsx
+++ src/components/ReposList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Alert } from 'antd';
 import Spinner from '../Spinner';
 /** Hooks, Services */
 import { useQuery } from 'graphql-hooks';
@@ -13,12 +13,17 @@ const ReposList: React.FC = () => {
   const { loading, error, data } = useQuery(SEARCH_REACT_LIBS_QUERY)
 
   if (loading) return <Spinner message="Loading repositories list..." />;
-  if (error) return (<div>{"error..."}</div>);
+  if (error) return <Alert
+    message="Server is too busy"
+    description="We are working hard to prepare repositories list for you. Please try to reload page one more time"
+    type="info"
+    showIcon={true}
+  />;
 
   const dataSource = processRepositoriesData(data);
   const columnsMap = getReposTableColumnsMap();
 
-  return (<Table dataSource={dataSource} columns={columnsMap}  rowKey="nodeIndex" />);
+  return <Table dataSource={dataSource} columns={columnsMap}  rowKey="nodeIndex" />;
 }
 
 export default ReposList;
